b433f0f1d6463b9525d7600813bafc50cb981515
Joshua Petersen <joshupetersen@gmail.com>
2019-09-24 12:35:11 -0700

    Update f_u_ruby.rb

--- f_u_ruby.rb
+++ f_u_ruby.rb
@@ -1,7 +1,17 @@
 class Hash
-  def do(Hash)
-    Hash.each do ||
-      
+  def do (*args)
+    case args.size
+      when 1
+        self.each do |x| end
+      when 2
+        self.each do |x, y| end
     end
+  end
+  def doing()
+end
+  
+class Array
+  def each
+    self do |x| end
   end
 end
